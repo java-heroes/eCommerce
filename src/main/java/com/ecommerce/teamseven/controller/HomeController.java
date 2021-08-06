3c8b813cde0040c863abce3da95829646067ca61
Ali Artuvan <81986977+aliartuvan@users.noreply.github.com>
2021-08-06 23:21:06 +0300

    Update HomeController.java

--- src/main/java/com/ecommerce/teamseven/controller/HomeController.java
+++ src/main/java/com/ecommerce/teamseven/controller/HomeController.java
@@ -5,7 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @Controller
 public class HomeController {
     /*
@@ -29,5 +28,4 @@ public class HomeController {
         }
         return "login";
     }
-
 }
