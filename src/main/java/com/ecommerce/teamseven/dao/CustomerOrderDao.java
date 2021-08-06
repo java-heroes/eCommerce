cbd3c6d2eea995597e599fe472393dfd4c908270
Ali Artuvan <81986977+aliartuvan@users.noreply.github.com>
2021-08-06 23:24:36 +0300

    Update CustomerOrderDao.java

--- src/main/java/com/ecommerce/teamseven/dao/CustomerOrderDao.java
+++ src/main/java/com/ecommerce/teamseven/dao/CustomerOrderDao.java
@@ -2,7 +2,6 @@ package com.ecommerce.teamseven.dao;
 
 import com.ecommerce.teamseven.model.CustomerOrder;
 
-
 public interface CustomerOrderDao {
     void addCustomerOrder(CustomerOrder customerOrder);
 }
