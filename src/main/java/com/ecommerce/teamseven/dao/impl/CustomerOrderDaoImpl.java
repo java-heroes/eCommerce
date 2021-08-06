2066daae6bf364080402369a726f76d1afd1aa21
Ali Artuvan <81986977+aliartuvan@users.noreply.github.com>
2021-08-06 23:24:24 +0300

    Update CustomerOrderDaoImpl.java

--- src/main/java/com/ecommerce/teamseven/dao/impl/CustomerOrderDaoImpl.java
+++ src/main/java/com/ecommerce/teamseven/dao/impl/CustomerOrderDaoImpl.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-
 @Repository
 @Transactional
 public class CustomerOrderDaoImpl implements CustomerOrderDao{
